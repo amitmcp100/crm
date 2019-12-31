b28d9573bed03bc8cc3e9741cf778022c7b74fcd
amitmcp100 <amit.mcp100@gmail.com>
2019-12-31 15:52:49 +0530

    run  component  sms code

--- runcampaign.php
+++ runcampaign.php
@@ -61,32 +61,12 @@ while ($row4 = $stmt->fetch()) {
 $mobile[]=$row4['mobile'];
 }
 }
-//print_r($mobile);
+
 $count_mobile=count($mobile);
-$mobiles= implode(",",$mobile);
+$mobile = implode(",",$mobile);
+$sms_text = $campaign_sms;
+include("sms_code.php");
 
-$curl = curl_init();
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "http://msg160.com/sendsms/send1_post",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"userid\"\r\n\r\nyogesh\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"pass\"\r\n\r\n12345678\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"sender\"\r\n\r\n".$sender_id."\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"mobile\"\r\n\r\n".$mobiles."\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"message\"\r\n\r\n".$campaign_sms."\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"route\"\r\n\r\n5\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--",
-  CURLOPT_HTTPHEADER => array(
-    "cache-control: no-cache",
-    "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
-    "postman-token: 2e642de6-bd3c-1d3f-b5ce-3acd25867c5b"
-  ),
-));
-
-$response = curl_exec($curl);
-//echo "sss".$sender_id;echo "</br>";
-//echo $sender_id;echo "</br>";
-//echo $response;
-//exit;
 if($response=='sms sent successfully'){
 	$update_sms=$available_sms-$count_mobile;
 	$u_sms=$used_sms+$count_mobile;
